df2e7d2a8642ecbc901737fc24240739720a0086
Mitchell Waite <mitch.thepcman@gmail.com>
2017-11-13 21:24:33 -0500

    Add notification support, exception handling, and debug info to python interface

--- KodiInterfacePython/kodi_interface.py
+++ KodiInterfacePython/kodi_interface.py
@@ -7,6 +7,9 @@ import sys
 import threading
 import time
 
+class KodiApiCommandFailureError(Exception):
+    pass
+
 class KodiInterface(object):
     def __init__(self, host, username, password, kodi_port):
         while True:
@@ -27,7 +30,7 @@ class KodiInterface(object):
             clt_thread.run()
 
     def _client_thread(self, clt_socket, clt_address):
-        print("Received connection from " + str(clt_address))	
+        print("Received connection from " + str(clt_address))
         sock_stream = clt_socket.makefile('r+')
         command_dict = { 'INPUT_UP' : self._input_up,
                          'INPUT_DOWN': self._input_down,
@@ -40,45 +43,57 @@ class KodiInterface(object):
                          'PLAYER_PLAY': self._player_play,
                          'PLAYER_PAUSE': self._player_pause,
                          'PLAYER_STOP': self._player_stop,
-                         'PLAYER_OPEN': self._player_open
+                         'PLAYER_OPEN': self._player_open,
+                         'GUI_NOTIFICATION': self._gui_notification
                        }
         for line in sock_stream:
             split_line = line.strip().split(' ')
             command = split_line[0]
             params = split_line[1:]
-			
-            print (command)
-			
+
             if command in command_dict:
-                command_dict[command](sock_stream, *params)
+                try:
+                    cmdEx = command_dict[command](sock_stream, *params)
+
+                    if u'error' in cmdEx.keys():
+                        print(cmdEx['error']['data']['stack']['message'])
+                        raise KodiApiCommandFailureError("shitfuck")
+                    else:
+                        print(command)
+
+                except Exception as e:
+                    print ("Command Error: {} - {}".format(type(e).__name__,str(e)))
             else:
                 sock_stream.write('Unrecognized command\n')
             sock_stream.flush()
         sock_stream.close()
         clt_socket.close()
-        		
+
         print("Connection from %s terminated" % (str(clt_address)))
 
+    def _gui_notification(self, sock_stream, tit, msg, tim):
+        return self.kodi.GUI.ShowNotification(title=tit, message=msg, displaytime="penis")
+
     def _input_up(self, sock_stream):
-        self.kodi.Input.Up()
+        return self.kodi.Input.Up()
 
     def _input_down(self, sock_stream):
-        self.kodi.Input.Down()
+        return self.kodi.Input.Down()
 
     def _input_right(self, sock_stream):
-        self.kodi.Input.Right()
+        return self.kodi.Input.Right()
 
     def _input_left(self, sock_stream):
-        self.kodi.Input.Left()
+        return self.kodi.Input.Left()
 
     def _input_select(self, sock_stream):
-        self.kodi.Input.Select()
+        return self.kodi.Input.Select()
 
     def _input_back(self, sock_stream):
-        self.kodi.Input.Back()
+        return self.kodi.Input.Back()
 
     def _input_home(self,sock_stream):
-        self.kodi.Input.Home()
+        return self.kodi.Input.Home()
 
     def _list_movies(self, sock_stream):
         for movie in self.kodi.VideoLibrary.GetMovies()['result']['movies']:
@@ -89,7 +104,8 @@ class KodiInterface(object):
             content_id = int(content_id)
         except ValueError:
             sock_stream.write('Content ID must be an integer\n')
-        self.kodi.Player.Open(item={'movieid':content_id})
+
+        return self.kodi.Player.Open(item={'movieid':content_id})
 
     def _player_play(self, sock_stream):
         player_ids = [rec['playerid'] for rec in self.kodi.Player.GetActivePlayers()['result']]
